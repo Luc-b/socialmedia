26fd30a525388274ebd265fb07400180fab16f53
Luc_b <lucijanabubnjic@gmail.com>
2024-03-14 22:55:46 +0100

    'social'

--- simplesocial/accounts/forms.py
+++ simplesocial/accounts/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import UserCreationForm
+#from django.contrib.auth.models import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 class UserCreateForm(UserCreationForm):
     
--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accounts',
+    'groups',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
+LOGIN_REDIRECT_URL='test'
+LOGOUT_REDIRECT_URL='thanks'
